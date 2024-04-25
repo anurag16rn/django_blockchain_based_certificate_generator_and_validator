173a4fb971a4f18e5cbd6b3c0f6be3724194f972
Anurag Kumar <anurag16rn@gmail.com>
2024-04-25 11:35:43 +0530

    cert1

--- generator/certifcate.py
+++ generator/certifcate.py
@@ -4,8 +4,8 @@ import random
 import os
 
 def write_name(
-        name: str,
         course: str,
+        name: str,
         date:str,
         template = 'template_1.jpg'
     ) -> str:
@@ -19,15 +19,34 @@ def write_name(
     # Save all the information we are going to use
     text_color = (50, 50, 50)  # Replace with the color you want the text to be
     
-    location_name = (400, 250)  # Replace with the coordinates you noted. (X, Y)
-    # location_course = (400, 350) 
-    # location_date = (xxx,yyy) 
+    #location_course = (334, 250)
+    location_course = (440, 250)  # Replace with the coordinates you noted. (X, Y)
 
-    font1 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 75) 
-    # font2 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 75)
+    # location_name = (440, 390) 
+    location_name = (350, 395) # Replace with the coordinates you noted. (X, Y)
+
+    # location_date = (xxx, yyy) 
+    location_date = (215, 485) # Replace with the coordinates you noted. (X, Y)
+    
+    # font1 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 50)
+    font1 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 50) 
+
+    # font2 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 50)
+    font2 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 50) 
+
+    # font3 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 45)
+    font3 = ImageFont.truetype(os.path.join(base_dir,"assets/fonts/font1.ttf"), 45) 
+    
+    
+    # d.text(location_date, date, fill=text_color, font=font1)
     d.text(location_name, name, fill=text_color, font=font1)
+
     # d.text(location_course, course, fill=text_color, font=font2)
-    # d.text(location_date, date, fill=text_color, font=font2)
+    d.text(location_course, course, fill=text_color, font=font2)
+
+    # d.text(location_date, date, fill=text_color, font=font3)
+    d.text(location_date, date, fill=text_color, font=font3)
+    
 
     # Create a unique name for the file. Feel free to edit the format, this is an example
     file_name = os.path.join(base_dir+"/assets/generated/") +  "_".join(name.split(' ')) + str(random.randint(0, 255)) + ".png"
@@ -38,4 +57,4 @@ def write_name(
     return file_name
 
 if __name__ == "__main__":
-    write_name("Raju Prakash Pant")
+    write_name("Core Java", "Raju Prakash Pant", "09-11-2001")
